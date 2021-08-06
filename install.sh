2c0a8c7160033e1ae372eb6de750a916c9f4790a
130N DEVELOPER <65381159+130N-STUDIO@users.noreply.github.com>
2021-08-05 22:02:04 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+termux-setup-storage
+pkg update && pkg upgrade
+pkg install git
+pkg install nodejs
+pkg install bash
+pkg install ffmpeg
+pkg install libwebp
+git clone https://github.com/130N-STUDIO/XYZbotV2
+cd XYZbotV2
+npm i
+node run.js
+echo [*] SCAN QR \nXYZteam \n XYZbot \n ThisLeonReal
